28fbc47257a6927d2e43d91f643a936b00e313b3
Eunchae Kim <kec9706@gmail.com>
2024-07-29 18:31:59 +0900

    feat: 코디 리스트 조회 관련 Serializer 구현

---
+++ outfit_posts/serializers.py
@@ -0,0 +1,62 @@
+from rest_framework import serializers
+from .models import OutfitPost, OutfitItem
+from brands.serializers import OutfitItemBrandSerializer
+from users.serializers import OutfitPostCreatorSerializer
+from celebrities.serializers import OutfitPostCelebritySerializer
+from users.models import CustomUser
+
+class BaseOutfitSerializer(serializers.Serializer):
+    scrap_count = serializers.SerializerMethodField()
+    is_scrapped = serializers.SerializerMethodField()
+
+    class Meta:
+        abstract = True
+        fields = [
+            "scrap_count",
+            "is_scrapped",
+        ]
+
+    def get_scrap_count(self, obj):
+        return obj.scraps.count()
+
+    def get_is_scrapped(self, obj):
+        user = self.context["request"].user
+        if user.is_anonymous:
+            return None
+
+        if not obj.scraps.filter(id=user.id):
+            return False
+
+        return True
+
+class OutfitItemSerializer(BaseOutfitSerializer, serializers.ModelSerializer):
+    brand = OutfitItemBrandSerializer()
+
+    class Meta:
+        model = OutfitItem
+        fields = [
+            "id",
+            "purchase_link",
+            "image_url",
+            *BaseOutfitSerializer.Meta.fields,
+            "brand",
+        ]
+
+class OutfitPostSerializer(BaseOutfitSerializer, serializers.ModelSerializer):
+    celebrity = OutfitPostCelebritySerializer()
+    creator = OutfitPostCreatorSerializer()
+    items = OutfitItemSerializer(many=True)
+
+    class Meta:
+        model = OutfitPost
+        fields = [
+            "id",
+            "title",
+            "created_at",
+            "gender",
+            "image_url",
+            *BaseOutfitSerializer.Meta.fields,
+            "celebrity",
+            "creator",
+            "items",
+        ]
