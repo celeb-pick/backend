95115cff2db6fe14dfd613dba67780cda2145041
Eunchae Kim <kec9706@gmail.com>
2024-07-29 14:26:30 +0900

    feat(model): Celebrity 모델에 누락된 category 필드 추가

--- celebrities/models.py
+++ celebrities/models.py
@@ -1,7 +1,26 @@
 from django.db import models
 
 class Celebrity(models.Model):
+    IDOL = "IDOL"
+    MODEL = "MODEL"
+    SINGER = "SINGER"
+    ACTOR = "ACTOR"
+    INFLUENCER = "INFLUENCER"
+    OTHERS = "OTHERS"
+    CATEGORY_CHOICES = (
+		(IDOL, "아이돌"),
+		(MODEL, "모델"),
+		(SINGER, "가수"),
+		(ACTOR, "배우"),
+		(INFLUENCER, "인플루언서"),
+		(OTHERS, "기타"),
+	)
+
     name = models.CharField(max_length=20)
+    category = models.CharField(
+		max_length=10,
+		choices=CATEGORY_CHOICES,
+	)
     profile_image_url = models.CharField(max_length=300)
     created_at = models.DateTimeField(auto_now_add=True)
 
