d40f8198f52f108bc8091443f4b01f0771a9b49a
Eunchae Kim <kec9706@gmail.com>
2024-07-29 20:01:27 +0900

    feat: 코디 리스트 조회 필터 추가

---
+++ outfit_posts/filters.py
@@ -0,0 +1,20 @@
+from .models import OutfitPost
+
+def get_filtered_outfit_posts(queryset, query_params):
+    gender = query_params.get("gender")
+    if gender:
+        queryset = queryset.filter(gender__in=[gender, OutfitPost.UNISEX])
+    
+    celebrity_category = query_params.get("celebrity_category")
+    if celebrity_category:
+        queryset = queryset.filter(celebrity__category=celebrity_category)
+    
+    search = query_params.get("search")
+    if search:
+        queryset = queryset.filter(title__contains=search)
+
+    item_id = query_params.get("item_id")
+    if item_id:
+        queryset = queryset.filter(items__id=item_id)
+    
+    return queryset
--- outfit_posts/views.py
+++ outfit_posts/views.py
@@ -2,10 +2,16 @@ from rest_framework import mixins
 from rest_framework import generics
 from .models import OutfitPost
 from .serializers import OutfitPostSerializer
+from .filters import get_filtered_outfit_posts
 
 class OutfitPostList(mixins.ListModelMixin, generics.GenericAPIView):
-    queryset = OutfitPost.objects.all()
     serializer_class = OutfitPostSerializer
+
+    def get_queryset(self):
+        queryset = OutfitPost.objects.all()
+        query_params = self.request.query_params
+
+        return get_filtered_outfit_posts(queryset, query_params)
 
     def get_serializer_context(self):
         context = super().get_serializer_context()
