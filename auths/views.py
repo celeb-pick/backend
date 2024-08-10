d473699e76221c36880910720820554da7f0f928
Eunchae Kim <kec9706@gmail.com>
2024-08-10 17:06:30 +0900

    feat: 인증, 로그인 API 구현

---
+++ auths/views.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import authenticate, login as auth_login
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(['POST'])
+def auth(request):
+    is_login = request.user.is_authenticated
+
+    return Response({"is_login": is_login}, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+def login(request):
+    error_message = "잘못된 유저 정보 입니다."
+    email = request.data.get("email")
+    password = request.data.get("password")
+
+    if email is None or password is None:
+        return Response({"message": error_message}, status=status.HTTP_400_BAD_REQUEST)
+
+    user = authenticate(request, email=email, password=str(password))
+    if user is None:
+        return Response({"message": error_message}, status=status.HTTP_400_BAD_REQUEST)
+
+    auth_login(request, user)
+    return Response(status=status.HTTP_204_NO_CONTENT)
