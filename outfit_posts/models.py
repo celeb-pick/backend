5ff1b11d5d1ea9b71e5de3af1d5041f32f61b80b
Eunchae Kim <123533586+lucaseunchae@users.noreply.github.com>
2024-08-22 01:12:20 +0900

    Feature/13 코디 추가 api 구현 (#14)
    * feat: Outfit Post 데이터 삭제시 many to many 테이블 데이터도 사라지도록 변경
    
    * feat: 코디 제목 글자수 최소 4자 제한 추가
    
    * chore(package): pillow, boto3, django-storages 패키지 추가
    
    * feat: 코디 추가 api 구현

--- outfit_posts/models.py
+++ outfit_posts/models.py
@@ -1,4 +1,14 @@
 from django.db import models
+from django.dispatch import receiver
+from django.core.validators import MinLengthValidator, FileExtensionValidator
+import uuid
+import os
+
+
+def generate_filename(value, filename):
+    extension = os.path.splitext(value.image.name)[1]
+    return f"outfit-post/{str(uuid.uuid4())}{extension}"
+
 
 class OutfitPost(models.Model):
 	MALE = "남성"
@@ -10,22 +20,45 @@ class OutfitPost(models.Model):
 		(UNISEX, "공용"),
 	)
 
-	items = models.ManyToManyField("outfit_posts.OutfitItem")
+	items = models.ManyToManyField("outfit_posts.OutfitItem", through="outfit_posts.OutfitPostItems")
 	celebrity = models.ForeignKey("celebrities.Celebrity", related_name="outfit_posts", on_delete=models.CASCADE)
 	creator = models.ForeignKey("users.CustomUser", related_name="outfit_posts", on_delete=models.SET_NULL, null=True)
-	title = models.CharField(max_length=20)
+	title = models.CharField(max_length=20, validators=[MinLengthValidator(4)])
 	created_at = models.DateTimeField(auto_now_add=True)
 	gender = models.CharField(
 		max_length=10,
 		choices=GENDER_CHOICES,
 	)
-	image_url = models.CharField(max_length=300)
+	image = models.ImageField(
+		upload_to=generate_filename,
+		validators=[FileExtensionValidator([
+			"jpg",
+			"jpeg",
+			"jfif",
+			"png",
+			"bmp",
+			"webp",
+			"tif",
+			"tiff",
+		])]
+	)
 
 	class Meta:
 		ordering = ["-created_at"]
 
 	def __str__(self):
 		return self.title
+
+
+@receiver(models.signals.pre_delete, sender=OutfitPost)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+		instance.image.delete()
+
+
+class OutfitPostItems(models.Model):
+    outfit_post = models.ForeignKey("outfit_posts.OutfitPost", on_delete=models.CASCADE)
+    outfit_item = models.ForeignKey("outfit_posts.OutfitItem", on_delete=models.CASCADE)
+
 
 class OutfitItem(models.Model):
 	TOP = "TOP"
--- outfit_posts/serializers.py
+++ outfit_posts/serializers.py
@@ -1,9 +1,12 @@
+from django.http import Http404
+from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 from .models import OutfitPost, OutfitItem
 from brands.serializers import OutfitItemBrandSerializer
 from users.serializers import OutfitPostCreatorSerializer
 from celebrities.serializers import OutfitPostCelebritySerializer
 from users.models import CustomUser
+from celebrities.models import Celebrity
 
 class BaseOutfitSerializer(serializers.Serializer):
     scrap_count = serializers.SerializerMethodField()
@@ -44,9 +47,16 @@ class TinyOutfitItemSerializer(BaseOutfitSerializer, serializers.ModelSerializer
         ]
 
 class OutfitPostSerializer(BaseOutfitSerializer, serializers.ModelSerializer):
-    celebrity = OutfitPostCelebritySerializer()
-    creator = OutfitPostCreatorSerializer()
-    items = TinyOutfitItemSerializer(many=True)
+    celebrity = OutfitPostCelebritySerializer(read_only=True)
+    creator = OutfitPostCreatorSerializer(read_only=True)
+    items = TinyOutfitItemSerializer(many=True, read_only=True)
+
+    celebrity_id = serializers.IntegerField(min_value=1, write_only=True)
+    item_ids = serializers.ListField(
+        child=serializers.IntegerField(min_value=1),
+        write_only=True,
+        allow_empty=False
+    )
 
     class Meta:
         model = OutfitPost
@@ -55,12 +65,47 @@ class OutfitPostSerializer(BaseOutfitSerializer, serializers.ModelSerializer):
             "title",
             "created_at",
             "gender",
-            "image_url",
+            "image",
             *BaseOutfitSerializer.Meta.fields,
             "celebrity",
             "creator",
             "items",
+            
+            # write onlys
+            "celebrity_id",
+            "item_ids",
         ]
+
+    def validate_celebrity_id(self, value):
+        celebrity = Celebrity.objects.filter(id=value).first()
+        if not celebrity:
+            raise serializers.ValidationError("올바르지 않은 셀럽 id값 입니다.")
+        
+        return value
+
+    def validate_item_ids(self, value):
+        if len(value) > 5:
+            raise serializers.ValidationError("코디 아이템은 5개 이하여야 합니다.")
+
+        items_count = OutfitItem.objects.filter(id__in=value).count()
+        if len(value) != items_count:
+            raise serializers.ValidationError("올바르지 않은 코디 아이템 id값이 포함되어 있습니다.")
+        
+        return value
+
+    def create(self, validated_data):
+        request = self.context['request']
+
+        item_ids = validated_data.pop('item_ids')
+        items = OutfitItem.objects.filter(id__in=item_ids)
+
+        celebrity_id = validated_data.pop('celebrity_id')
+        celebrity = Celebrity.objects.get(id=celebrity_id)
+
+        outfit_post = OutfitPost.objects.create(**validated_data, celebrity=celebrity, creator=request.user)
+        outfit_post.items.set(items)
+        
+        return outfit_post
 
 
 class OutfitItemSerializer(BaseOutfitSerializer, serializers.ModelSerializer):
