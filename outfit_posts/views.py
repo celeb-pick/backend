996633b8c51309f930035359043ec433b845f6d6
Eunchae Kim <kec9706@gmail.com>
2024-08-26 17:26:37 +0900

    feat: api 구현

--- outfit_posts/views.py
+++ outfit_posts/views.py
@@ -2,9 +2,9 @@ from rest_framework import permissions
 from rest_framework import mixins
 from rest_framework import generics
 from rest_framework.parsers import FormParser, MultiPartParser
-from .models import OutfitPost
-from .serializers import OutfitPostSerializer
-from .filters import get_filtered_outfit_posts
+from .models import OutfitPost, OutfitItem
+from .serializers import OutfitPostSerializer, OutfitItemSerializer
+from .filters import get_filtered_outfit_posts, get_filtered_outfit_items
 
 class OutfitPostList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
     serializer_class = OutfitPostSerializer
@@ -28,3 +28,22 @@ class OutfitPostList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.Ge
 
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
+
+
+class OutfitItemList(mixins.ListModelMixin, generics.GenericAPIView):
+    serializer_class = OutfitItemSerializer
+
+    def get_queryset(self):
+        queryset = OutfitItem.objects.all()
+        query_params = self.request.query_params
+
+        return get_filtered_outfit_items(queryset, query_params)
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context.update({"request": self.request})
+        
+        return context
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
